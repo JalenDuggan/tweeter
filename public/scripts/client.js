52a7eef34eca9932d5cc8ef342d19485d70dbb2f
Jalen Duggan <jalen.j.duggan@gmail.com>
2021-08-17 22:28:24 +0000

    Implented new way to change color of reactions to yellow with css hover

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,30 +5,6 @@
  */
 
 // Test / driver code (temporary). Eventually will get this from the server.
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
 
 
 $(document).ready(function() {
@@ -115,8 +91,8 @@ const createTweetElement = function (tweetObj) {
     <h5>
       ${timeago.format(tweetObj.created_at)}
     </h5>
-    <div>
-      <i id="test"class="fas fa-flag react"></i>
+    <div class="test">
+      <i class="fas fa-flag react"></i>
       <i class="fas fa-retweet react"></i>
       <i class="fas fa-heart react"></i>
     </div>
